e0c6f89b03984063705f3a568d55e78c9e2e0019
kmach <kmach@uw.edu>
2019-06-09 16:07:49 -0700

    server update

--- src/Server.ts
+++ src/Server.ts
@@ -5,15 +5,26 @@ const PORT = 8080;
 //The server initiates listening once instantiated
 const server = new WebSocket.Server({port: PORT});
 console.log(`Started new WebSocket server on ${PORT}`)
-
+let clients: WebSocket[] = [];
 //when receiving a connection from a client
 server.on('connection', (client) => {
   console.log("Log: new connection!")
   client.send('Welcome to the server!');
-
+  clients.push(client);
   //event handler for ALL messages (from that client)
   client.on('message', (message: string) => {
       console.log(`log: received ${message}`)
+      console.log('log: ' + sendText(client, message, clients.indexOf(client)));
+      
       client.send(`You said: "${message}"`); //echo back
   });
 });
+
+function sendText(client: WebSocket, message: string, playerID: number) {
+  let msg = {
+    cmd: "commands",
+    id: playerID,
+    text: message
+  }
+  client.send(JSON.stringify(msg));
+}
